83185d83571020b6d0b48b6405f8cde466f8ddba
NoLuck-ET <lca9201@g.rit.edu>
2023-05-16 21:49:35 -0400

    update

--- alexiou-l-hw3/src/main.js
+++ alexiou-l-hw3/src/main.js
@@ -2,17 +2,60 @@ import { MyBookmark } from "./myBookmark.js";
 import { Favorite } from "./favorite.js";
 
 let favorites = [];
-let favorite = new Favorite(crypto.randomUUID(), "RIT", "hhtps://www.rit.edu", "A private university located near Rochester, NY.");
+let favorite = new Favorite("d2e7e357-1b1f-4eea-b8f9-25af8aa17138", "RIT", "https://www.rit.edu", "A private university located near Rochester, NY.");
 console.log(favorite);
 favorites.push(favorite);
 
 const submitClicked = (evt) => {
   console.log("submitClicked");
+  let text = document.querySelector("#favorite-text").value.trim();
+  let url = document.querySelector("#favorite-url").value.trim();
+  let comment = document.querySelector("#favorite-comments").value.trim();
+  if(text.length > 0 && url.length > 0 && comment.length > 0){
+    let submission = new Favorite(crypto.randomUUID(), text, url, comment);
+    favorites.push(submission);
+    createBookmarkComponent(submission);
+  }else{
+    //Website seems to automatically produce an error message on-screen, so I'll just make a console one
+    console.log("ERROR: Missing fields");
+    evt.preventDefault();
+    return false;
+  }
   
+
+  evt.preventDefault();
+  return false;
+};
+
+/*
+const clearFormFields = (evt) => {
+  console.log("clearFormFields");
+  document.querySelector("#favorite-text").value = "";
+  document.querySelector("#favorite-url").value = "";
+  document.querySelector("#favorite-comments").value = "";
+
+  evt.preventDefault();
+  return false;
+};*/
+
+const clearFormFields = (evt) => {
+  console.log("clearFormFields");
+  document.querySelector("#favorite-text").value = "";
+  document.querySelector("#favorite-url").value = "";
+  document.querySelector("#faovrite-comments").value = "";
   
   evt.preventDefault();
   return false;
 };
+
+document.querySelector("#favorite-submit-button").onclick = (evt) => {
+  submitClicked(evt);
+};
+
+document.querySelector("#favorite-cancel-button").onclick = (evt) => {
+  clearFormFields(evt);
+};
+
 
 const createBookmarkComponent = (sample) => { //fid, text, url, comments
   let bookmark = document.createElement("my-bookmark");
@@ -25,7 +68,7 @@ const createBookmarkComponent = (sample) => { //fid, text, url, comments
   const newLI = document.createElement("li");
   newLI.appendChild(bookmark);
   document.querySelector("#bookmarks").appendChild(newLI);
-  console.log("help");
+  //console.log("help");
 };
 
 const loadFavoritesFromStorage = () => {
@@ -35,75 +78,10 @@ const loadFavoritesFromStorage = () => {
   }
 };
 
-const clearFormFields = (evt) => {
-  console.log("clearFormFields");
-  document.querySelector("#favorite-text").value = "";
-  document.querySelector("#favorite-url").value = "";
-  document.querySelector("#favorite-comments").value = "";
-  
-  evt.preventDefault();
-  return false;
-};
+createBookmarkComponent(new Favorite("58dfh18929", "pain", "https://.pain.com", "Terrible website"));
 
-const init = () => {
-  document.querySelector("#favorite-submit-button").onclick = submitClicked;
-  document.querySelector("#favorite-cancel-button").onclick = clearFormFields;
 
-  //createBookmarkComponent("58dfh18929", "pain", "https://.pain.com", "Terrible website");
+
+window.onload = () => {
   loadFavoritesFromStorage();
-  //let bookmarkHTML = document.querySelector("#bookmarks");
-//     for(let b of bookmarks){
-//         let book = new MyBookmark;
-//         book._text = b.text;
-//         book._url = b.url;
-//         book._comments = b.comments;
-//         book.render();
-//         bookmarkHTML.innerHTML += `<li><my-bookmark data-text="${book._text}" data-url="${book._url}" data-comments="${book._comments}">${book.innerHTML}</my-bookmark></li>`;
-//         //bookmarkHTML.innerHTML += `<li><my-bookmark>${book.innerHTML}</my-bookmark></li>`;
-//     }
 };
-
-init();
-
-// const bookmarks = [
-//     {
-//       text: "Bing",
-//       url: "https://www.bing.com",
-//       comments: "Bing is a web search engine owned and operated by Microsoft."
-//     },
-//     {
-//       text: "Google",
-//       url: "https://www.google.com",
-//       comments: "Google Search is a search engine provided and operated by Google."
-//     },
-//     {
-//       text: "DuckDuckGo",
-//       url: "https://duckduckgo.com/",
-//       comments: "DuckDuckGo (DDG) is an internet search engine that emphasizes protecting searchers' privacy."
-//     }
-// ];
-
-// window.onload = () => {
-//     // // Create a MyBookmark and add it to the list
-//     // const bing = document.createElement("my-bookmark");
-
-//     // // ANOTHER way to set custom attributes, the .dataset property
-//     // // note that these 2 lines of code will also trigger attributeChangedCallback()
-//     // bing.dataset.text = "Bing";
-//     // bing.dataset.url = "https://www.bing.com/";
-
-//     // const newLI = document.createElement("li");
-//     // newLI.appendChild(bing);
-//     // document.querySelector("#bookmarks").appendChild(newLI);
-
-//     let bookmarkHTML = document.querySelector("#bookmarks");
-//     for(let b of bookmarks){
-//         let book = new MyBookmark;
-//         book._text = b.text;
-//         book._url = b.url;
-//         book._comments = b.comments;
-//         book.render();
-//         bookmarkHTML.innerHTML += `<li><my-bookmark data-text="${book._text}" data-url="${book._url}" data-comments="${book._comments}">${book.innerHTML}</my-bookmark></li>`;
-//         //bookmarkHTML.innerHTML += `<li><my-bookmark>${book.innerHTML}</my-bookmark></li>`;
-//     }
-// };
